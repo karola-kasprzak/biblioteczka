c221ca1d0b124f04415ea1cb50aecdd1a7b31aef
karola-kasprzak <karola.a.kasprzak@gmail.com>
2020-10-28 14:39:57 +0100

    "svg won't print correctly"

--- main.js
+++ main.js
@@ -122,8 +122,7 @@ function createCard(element) {
 
   const bookDiv = createElement("div", "class", "card m-2");
   bookDiv.style = "width: 300px;";
-  console.log(bookDiv);
-
+  
   const bookImg = createElement("img", "class", "card-img-top");
   bookImg.src = element.img;
   bookImg.style = "width: 298px; height: 298px;";
@@ -141,13 +140,27 @@ function createCard(element) {
 
   const bookScoreDiv = createElement("div", "class", "mt-0");
 
-  // const bookScoreLabel = createElement("span", "class", "pr-2");
-  // bookScoreLabel.innerHTML = `Ocena: ${element.score}`;
-  // na razie bez gwiazdek
-  bookScoreDiv.innerHTML = printStars(element.score);
+  // Tworzenie napisu "Ocena: "
+  const bookScoreLabel = createElement("span", "class", "pr-2");
+  bookScoreLabel.innerHTML = "Ocena: ";
+  bookScoreDiv.appendChild(bookScoreLabel);
 
+  // Drukowanie gwiazdek
+  const numYellowStars = element.score;
+  const numGrayStars = 6 - element.score;
 
-  // bookScoreDiv.appendChild(bookScoreLabel);
+  const bookStarYellow = drawStar("star--yellow");
+  console.log(bookStarYellow)
+  bookScoreDiv.appendChild(bookStarYellow);
+
+  // for (let i = 0; i <= numYellowStars; i++) {
+  //   bookScoreDiv.appendChild(drawStar("star--yellow"));
+  // }
+
+  // for (let i = 0; i <= numGrayStars; i++) {
+  //   bookScoreDiv.appendChild(drawStar("star--gray"));
+  // }
+
   bookDivBody.appendChild(bookTitle);
   bookDivBody.appendChild(bookAuthor);
   bookDivBody.appendChild(bookGenre);
@@ -158,32 +171,38 @@ function createCard(element) {
   return rootDiv.appendChild(bookDiv);
 }
 
-function printStars(score) {
-  const bookScoreLabel = createElement("span", "class", "pr-2");
-  bookScoreLabel.innerHTML = "Ocena: ";
+// function printStars(score) {
+//   const bookScoreLabel = createElement("span", "class", "pr-2");
+//   bookScoreLabel.innerHTML = "Ocena: ";
 
-  const bookStar__pathTag = createElement("path", "d", 'M511.267,197.258c-1.764-5.431-6.457-9.389-12.107-10.209l-158.723-23.065L269.452,20.157 c-2.526-5.12-7.741-8.361-13.45-8.361c-5.71,0-10.924,3.241-13.451,8.361l-70.988,143.827l-158.72,23.065 c-5.649,0.82-10.344,4.778-12.108,10.208c-1.765,5.431-0.293,11.392,3.796,15.377l114.848,111.954L92.271,482.671 c-0.966,5.628,1.348,11.314,5.967,14.671c2.613,1.898,5.708,2.864,8.818,2.864c2.388,0,4.784-0.569,6.978-1.723l141.967-74.638 l141.961,74.637c5.055,2.657,11.178,2.215,15.797-1.141c4.619-3.356,6.934-9.044,5.969-14.672l-27.117-158.081l114.861-111.955 C511.56,208.649,513.033,202.688,511.267,197.258z');
   
-  const bookStar__gTag = createElement("g","class","");
+//   const bookStarYellow = drawStar("star--yellow");
+//   console.log("printStars -> bookStarYellow", bookStarYellow)
+
+  
+//   const bookStarGray = drawStar("star--gray");
+//   console.log("printStars -> bookStarGray", bookStarGray)
+  
+//   const numYellowStars = score;
+//   const numGrayStars = 6 - score;
+
+//   return bookScoreLabel.innerText + bookStarYellow
+// }
+
+function drawStar (colorClass) {
+  let ns = 'http://www.w3.org/2000/svg';
+  const bookStar__pathTag = document.createElementNS(ns, "path")
+  bookStar__pathTag.setAttribute("d", "M100 100 L100 0 A100 100 0 0 1 187 50 Z");
+  
+  // const bookStar__gTag = document.createElement("g");
   const bookStar__svgTag = createElement("svg", "viewBox", "0 0 512.002 512.002");
 
-  bookStar__gTag.appendChild(bookStar__pathTag);
-  bookStar__svgTag.appendChild(bookStar__gTag);
+  // bookStar__gTag.appendChild(bookStar__pathTag);
+  bookStar__svgTag.appendChild(bookStar__pathTag);
 
-  const bookStarYellow = bookStar__svgTag;
-  bookStarYellow.classList.add("star", "star--yellow");
-  
-  const bookStarGray = bookStar__svgTag;
-  // bookStarGray.classList.add("star", "star--gray");
-  
-  const numYellowStars = score;
-  const numGrayStars = 6 - score;
-
-  console.log(bookStar__svgTag);
-  console.log(bookStarYellow);
-  return bookScoreLabel.innerText
+  bookStar__svgTag.classList.add("star", colorClass);
+  return bookStar__svgTag
 }
-
 
 const addBtn = document.querySelector('.box--btn__add');
 addBtn.addEventListener("click", addBook);
